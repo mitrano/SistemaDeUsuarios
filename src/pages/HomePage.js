b99fefe94887b391bdf56358c5c5c57c6001d0d5
Ricardo Mitrano <mitrano@gmail.com>
2024-12-02 17:52:09 -0300

    Inclusão de Imagem da Home Page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 
 const HomePage = () => (
   <div>    
-    <h1>Bem vindo ao sistema de Gestão de Usuários</h1>
+    <h1>Bem-vindo ao sistema de Gestão de Usuários!</h1>
+    <img 
+      src="/images/user_registration_homepage.jpg" 
+      alt="Homepage Sistema de Usuários" 
+      style={{ width: '100%', height: 'auto', marginTop: '20px' }} 
+    />
   </div>
+  
 );
 
 export default HomePage;
